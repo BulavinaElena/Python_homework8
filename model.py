bf458a76aca14407ccea49d372dbadb4db3463b5
Bulavina Elena <koroleva488@mail.ru>
2022-11-06 15:50:55 +1000

    model

---
+++ model.py
@@ -0,0 +1,50 @@
+import controller
+
+phone_book = []
+path = 'data/'
+
+def get_phone_book():
+    global phone_book
+    return phone_book
+
+def set_path(file):
+    global path 
+    path += file
+
+def open_file():
+    global path 
+    global phone_book
+    with open (path, 'r', encoding='UTF-8') as file:
+        data = file.readlines()
+    for item in data:
+        contact = item.replace('\n', '').split(';')
+        phone_book.append(contact)
+
+def new_contact(contact):
+    global phone_book
+    phone_book.append(list(contact))
+
+def change_contact(id, choise, value):
+    global phone_book
+    phone_book [int(id)][int(choise)] = value
+
+def del_contact(id):
+    global phone_book
+    phone_book.pop(id)
+        
+def search_contact(id, value):
+    global phone_book
+    global search_phone_book
+    for item in phone_book:
+        if item[int(id)] ==  value:
+            search_phone_book.append(item)
+
+def get_search_phone_book():
+    global search_phone_book
+    return search_phone_book 
+
+def record_file(path):
+    global phone_book
+    with open(path, 'w', encoding='UTF-8') as file:
+            file.write(str(phone_book))
+            
